df40ef57fcf9c5ded728a3358e6653466cd1eb07
yla382 <yoonhlee0831@gmail.com>
2022-01-23 13:18:50 -0800

    comments added

--- work/as1/fileHandler.h
+++ work/as1/fileHandler.h
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Write to the file
+input: char*, char*
+output: void
+*/
 void setFile(char *fileName, char *value);
+
+/*
+Read the file and retrieve the file content
+input: char*
+output: char
+*/
 char getFileContent(char *fileName);
