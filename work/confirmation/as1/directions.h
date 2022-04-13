f5e6e5d2ccbd04f8295b4f9cefa1b5b5e68d5319
John Lee <60329514+yla382@users.noreply.github.com>
2022-04-12 23:07:09 -0700

    Delete work/confirmation directory

--- work/confirmation/as1/directions.h
+++
@@ -1,6 +0,0 @@
-#ifndef DIRECTIONS_H
-#define DIRECTIONS_H
-
-typedef enum {LEFT, RIGHT, UP, DOWN, INVALID} directions;
-
-#endif
--- work/confirmation/as2/run.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "light_sampler.h"
-
-int main() {
-	printf("Starting to sample data...\n");
-	light_sampler(); //Start sampling light voltage from beaglebone
-	printf("Cleaning up...4321\nDone!\n");
-}
